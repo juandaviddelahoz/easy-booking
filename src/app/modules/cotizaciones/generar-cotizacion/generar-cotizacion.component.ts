3647841e948043c3fd0864f2cddadfdd3087d7fa
juandaviddelahoz <juandavid.delahoz@viajarltda.com>
2022-01-03 17:47:53 -0500

    Prueba libreria HTML a PDF como imagen

--- src/app/modules/cotizaciones/generar-cotizacion/generar-cotizacion.component.ts
+++ src/app/modules/cotizaciones/generar-cotizacion/generar-cotizacion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as html2pdf from 'html2pdf.js';
 
 @Component({
   selector: 'app-generar-cotizacion',
@@ -10,6 +11,23 @@ export class GenerarCotizacionComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onExportClick(){
+    const options = {
+      margin: 0.2,
+      filename: 'Cotizacion_1-pdf',
+      image: {type: 'jpeg', quality: 5},
+      html2canvas: {},
+      jsPDF: {unit: 'in', format: 'letter', orientation: 'portrait'}
+    };
+
+    const content: Element = document.getElementById('element-to-export');
+
+    html2pdf()
+    .from(content)
+    .set(options)
+    .save();
   }
 
 }
