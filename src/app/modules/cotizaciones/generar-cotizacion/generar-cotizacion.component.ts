d391f9a3dd58bf6d2f07e0deb3a66abcf248afd0
Juan David De la Hoz <juandavid.delahoz@viajar.intra>
2022-01-07 15:10:06 -0500

    Prueba libreria HTML a PDF como imagen alta resolucion

--- src/app/modules/cotizaciones/generar-cotizacion/generar-cotizacion.component.ts
+++ src/app/modules/cotizaciones/generar-cotizacion/generar-cotizacion.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import * as html2pdf from 'html2pdf.js';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+import breakingBad from './fake-data.json';
 
 @Component({
   selector: 'app-generar-cotizacion',
@@ -8,26 +10,48 @@ import * as html2pdf from 'html2pdf.js';
 })
 export class GenerarCotizacionComponent implements OnInit {
 
-  constructor() { }
+  breakingBadData = breakingBad;
+
+  constructor() {
+    // this.downloadPDF();
+    console.log(this.breakingBadData)
+  }
 
   ngOnInit(): void {
   }
 
-  onExportClick(){
+  public downloadPDF() {
+    // Extraemos el
+    const DATA = document.getElementById('htmlData');
+    const doc = new jsPDF('p', 'pt', 'a4');
     const options = {
-      margin: 0.2,
-      filename: 'Cotizacion_1-pdf',
-      image: {type: 'jpeg', quality: 5},
-      html2canvas: {},
-      jsPDF: {unit: 'in', format: 'letter', orientation: 'portrait'}
+      background: 'white',
+      scale: 3
     };
+    html2canvas(DATA, options).then((canvas) => {
 
-    const content: Element = document.getElementById('element-to-export');
+      const img = canvas.toDataURL('image/PNG');
 
-    html2pdf()
-    .from(content)
-    .set(options)
-    .save();
+      // Add image Canvas to PDF
+      const bufferX = 15;
+      const bufferY = 15;
+      const imgProps = (doc as any).getImageProperties(img);
+      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      doc.addImage(
+        img,
+        'PNG', 
+        bufferX, 
+        bufferY, 
+        pdfWidth, 
+        pdfHeight, 
+        undefined, 
+        'FAST'
+      );
+      return doc;
+    }).then((docResult) => {
+      docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
+    });
   }
 
 }
